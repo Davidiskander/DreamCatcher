d29dbd14c0fc66f911fd29fd9309f78da3c76c6c
David Iskander <david.iskander.di@gmail.com>
2016-03-12 17:02:01 -0800

    first commit

---
+++ Dream Catcher/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Dream Catcher
+//
+//  Created by David Iskander on 3/12/16.
+//  Copyright © 2016 DIskander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
